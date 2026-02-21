f7c433b6284bb1e1d5162ee26712f52dadcfadc9
brett-me <[its-me-brett@protonmail.com]>
2026-02-21 13:36:05 +0200

    add: room 4 trapped trigger

--- states/play.py
+++ states/play.py
@@ -29,10 +29,12 @@ class PlayState:
         self.corridor_sealed = False
         self.door12_closed = False
         self.door23_closed = False
+        self.room4_trapped = False
 
         self.map.set_group_active("!", False)
         self.map.set_group_active("@", False)
         self.map.set_group_active("%", False)
+        self.map.set_group_active("*", False)
 
         self.suspicion = Suspicion()
         self.suspicion.value = starting_suspicion
@@ -100,6 +102,13 @@ class PlayState:
             if self.map.is_trigger(cell, "V"):
                 self.door23_closed = True
                 self.map.set_group_active("%", True)
+                self.walls = self.map.get_walls()
+
+        if self.player and (not self.room4_trapped):
+            cell = self.map.world_to_cell(self.player.rect.centerx, self.player.rect.centery)
+            if self.map.is_trigger(cell, "W"):
+                self.room4_trapped = True
+                self.map.set_group_active("*", True)
                 self.walls = self.map.get_walls()
 
     def draw_grid(self, screen):
